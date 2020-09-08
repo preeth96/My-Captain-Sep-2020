e1c663364292747684c8157a06ce485e8dbbb3da
preeth96 <65329452+preeth96@users.noreply.github.com>
2020-09-08 21:19:14 +0530

    Create Productoftwonumbers.cpp

---
+++ Productoftwonumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    float a, b;
+    cout << "Enter two numbers: ";
+    cin >> a>>b;
+    
+  
+   
+    cout <<"Product=" <<float(a*b)<< endl;
+    return 0;
+}
